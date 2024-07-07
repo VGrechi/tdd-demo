8d0897d69c4ddb68f9ebbb4f693eea17098c0831
Victor Grechi <victor.grechi@superbid.net>
2024-07-07 15:39:58 -0300

    feat: impl 1

--- src/main/java/com/grechi/tdddemo/service/SaleNoteService.java
+++ src/main/java/com/grechi/tdddemo/service/SaleNoteService.java
@@ -5,6 +5,8 @@ import com.grechi.tdddemo.repository.SaleNoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class SaleNoteService {
 
@@ -24,6 +26,9 @@ public class SaleNoteService {
                 .saleId(saleId)
                 .build();
 
+        Long nextNumber = getNextNumber();
+        saleNote.setNumber(nextNumber);
+
         byte[] pdf = fileService.generatePDF(saleNote);
 
         uploadService.uploadFile(saleNote, pdf);
@@ -31,5 +36,16 @@ public class SaleNoteService {
         saleNoteRepository.save(saleNote);
 
         return saleNote;
+    }
+
+    private Long getNextNumber() {
+        long nextNumber = 1L;
+        Optional<SaleNote> opLastSaleNote = saleNoteRepository.getLastSaleNote();
+
+        if(opLastSaleNote.isPresent()){
+            nextNumber = opLastSaleNote.get().getNumber() + 1L;
+        }
+
+        return nextNumber;
     }
 }
