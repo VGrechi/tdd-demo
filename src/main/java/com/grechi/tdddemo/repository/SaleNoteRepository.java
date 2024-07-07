be8dfdce5ce7b6f30bf81bed7075c44ea3d2587a
Victor Grechi <victor.grechi@superbid.net>
2024-07-07 15:39:58 -0300

    feat: impl 1

--- src/main/java/com/grechi/tdddemo/repository/SaleNoteRepository.java
+++ src/main/java/com/grechi/tdddemo/repository/SaleNoteRepository.java
@@ -2,6 +2,13 @@ package com.grechi.tdddemo.repository;
 
 import com.grechi.tdddemo.models.SaleNote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface SaleNoteRepository extends JpaRepository<SaleNote, Long> {
+
+    @Query(nativeQuery = true,
+    value = "SELECT * FROM SaleNote ORDER BY NUMBER DESC")
+    Optional<SaleNote> getLastSaleNote();
 }
